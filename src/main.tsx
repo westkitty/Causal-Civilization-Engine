ec255900ab5556fe42f9e1349c3d74be4ee6a593
westkitty <andrew.dolby@westcat.ca>
2026-07-22 10:21:22 -0400

    fix: restore app entrypoint with standard orbit policy

--- src/main.tsx
+++ src/main.tsx
@@ -8,4 +8,10 @@ import './observatory-shell.css'
 import './rendering/standardOrbitControlsPolicy'
 import './rendering/instancedMeshUploadPolicy'
 import './rendering/rendererVisualPolicy'
-import App
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
