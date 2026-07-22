25d1b1ea73ca8c31f430f409d5914f20ff3de991
westkitty <andrew.dolby@westcat.ca>
2026-07-22 05:10:55 -0400

    feat: activate cinematic renderer policy

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './visual-overhaul.css'
 import './map-observatory.css'
 import './map-scan-effects.css'
 import './observatory-shell.css'
+import './rendering/rendererVisualPolicy'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
