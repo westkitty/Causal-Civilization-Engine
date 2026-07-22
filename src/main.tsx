00a89ecb82bf5887bbd8c86371afc12cfaa03495
westkitty <andrew.dolby@westcat.ca>
2026-07-22 04:55:26 -0400

    style: activate map observatory presentation layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './visual-overhaul.css'
+import './map-observatory.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
