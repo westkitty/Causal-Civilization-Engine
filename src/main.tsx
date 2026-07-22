10a63a3a3724e7a315843b15687580bb271bc080
westkitty <andrew.dolby@westcat.ca>
2026-07-22 11:15:15 -0400

    feat: add observatory visual overhaul and cinematic map rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './visual-overhaul.css'
+import './map-observatory.css'
+import './map-scan-effects.css'
+import './observatory-shell.css'
+import './rendering/instancedMeshUploadPolicy'
+import './rendering/rendererVisualPolicy'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
