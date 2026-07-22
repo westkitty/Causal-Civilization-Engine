48a7b0d845ead224dfa6ed10254333f146f4b7fd
westkitty <andrew.dolby@westcat.ca>
2026-07-22 05:02:35 -0400

    fix: activate observatory scan effects

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './visual-overhaul.css'
 import './map-observatory.css'
+import './map-scan-effects.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
