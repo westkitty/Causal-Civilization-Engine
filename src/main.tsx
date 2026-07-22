263435c617fe46a0d7f325d2f050c21920b1ea7b
westkitty <andrew.dolby@westcat.ca>
2026-07-22 10:38:40 -0400

    feat: launch playable civilization mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import './visual-overhaul.css'
 import './map-observatory.css'
 import './map-scan-effects.css'
 import './observatory-shell.css'
+import './playable.css'
 import './rendering/standardOrbitControlsPolicy'
 import './rendering/instancedMeshUploadPolicy'
 import './rendering/rendererVisualPolicy'
-import App from './App.tsx'
+import PlayableApp from './PlayableApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PlayableApp />
   </StrictMode>,
 )
